2d53485ab902cf6186cf2efb9dca02518977b860
“mawar” <jonathanfrizy12@gmail.com>
2025-06-21 19:42:44 +0700

    perubahan

--- app/src/main/java/com/example/careerpartner/MainActivity.kt
+++ app/src/main/java/com/example/careerpartner/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.careerpartner
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        navController.addOnDestinationChangedListener({_, destination, _ ->
+            val bottomDestinations = setOf(
+                R.id.homeFragment,
+                R.id.discoverFragment,
+                R.id.profileFragment)
+            bottomNav.visibility = if (destination.id in bottomDestinations) View.VISIBLE else View.GONE
+        })
+
+        bottomNav.setupWithNavController(navController)
     }
 }
