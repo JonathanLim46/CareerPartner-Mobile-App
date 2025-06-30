f22832580d96c0784318d1cb713dea7f0b9adb0b
“mawar” <jonathanfrizy12@gmail.com>
2025-06-30 09:21:34 +0700

    refactor: binding view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.careerpartner"
     compileSdk = 35
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.careerpartner"
         minSdk = 33
