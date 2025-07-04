ee054507c08cfa1bd78fdaf83f2d24b1e4126644
“mawar” <jonathanfrizy12@gmail.com>
2025-07-04 18:18:37 +0700

    refactor id

--- app/src/main/java/com/example/careerpartner/main/profile/screen/SettingsFragment.kt
+++ app/src/main/java/com/example/careerpartner/main/profile/screen/SettingsFragment.kt
@@ -5,16 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.careerpartner.R
+import com.example.careerpartner.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+
+    private var _binding : FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAbout.setOnClickListener{
+            findNavController().navigate(R.id.action_settingsFragment_to_aboutFragment)
+        }
+
+        binding.btnSupportHelp.setOnClickListener{
+            findNavController().navigate(R.id.action_settingsFragment_to_supportHelpFragment)
+        }
+
+        binding.btnGradientButton.setOnClickListener{
+            findNavController().navigate(R.id.action_settingsFragment_to_accountFragment)
+        }
+
+        binding.btnBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 
 }
