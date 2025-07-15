56d4621feb4bcb27d3ff96b0cdb510e1b2969338
“mawar” <jonathanfrizy12@gmail.com>
2025-07-16 06:42:04 +0700

    feat: route internship detail

--- app/src/main/java/com/example/careerpartner/data/network/InternshipApi.kt
+++ app/src/main/java/com/example/careerpartner/data/network/InternshipApi.kt
@@ -4,11 +4,15 @@ import com.example.careerpartner.data.model.InternshipsResponse
 import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 
 interface InternshipApi {
 
     @GET("api/talent/internships")
     suspend fun getInternshipsData(@Header("Authorization") token: String): retrofit2.Response<InternshipsResponse>
+
+    @GET("api/talent/internships/{id}")
+    suspend fun getInternshipsDetail(@Header("Authorization") token: String, @Path(value = "id") id: Int): retrofit2.Response<InternshipsResponse>
 
     companion object{
         fun getApi(): InternshipApi? {
