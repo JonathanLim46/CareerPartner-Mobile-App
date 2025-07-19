9ab3172652dc402346f7caa8e2ca9bf09661eb0d
“mawar” <jonathanfrizy12@gmail.com>
2025-07-20 00:19:20 +0700

    feat: shimmer profile

--- app/src/main/java/com/example/careerpartner/main/profile/screen/ProfileMyCareerFragment.kt
+++ app/src/main/java/com/example/careerpartner/main/profile/screen/ProfileMyCareerFragment.kt
@@ -1,16 +1,23 @@
 package com.example.careerpartner.main.profile.screen
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.careerpartner.R
+import com.example.careerpartner.data.network.BaseResponse
+import com.example.careerpartner.data.viewmodel.UserViewModel
 import com.example.careerpartner.databinding.FragmentProfileMyCareerBinding
 import com.example.careerpartner.main.profile.adapter.ProfileAdapter
 import com.example.careerpartner.main.profile.data.ProfileCareerData
+import java.text.NumberFormat
+import java.util.Locale
 
 class ProfileMyCareerFragment : Fragment() {
 
@@ -21,6 +28,8 @@ class ProfileMyCareerFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var profileData: List<ProfileCareerData>
     private lateinit var rawData: List<List<String>>
+
+    private val viewModelUser: UserViewModel by activityViewModels<UserViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,11 +49,39 @@ class ProfileMyCareerFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         recyclerView = binding.rvMyCareerPath
         setupDataRv()
+
+        viewModelUser.getTalentData(requireActivity())
+
+        viewModelUser.userResult.observe(viewLifecycleOwner) {
+            when (it) {
+                is BaseResponse.Loading -> {
+                    binding.layoutMyCareer.visibility = View.GONE
+                    binding.shimmerLayout.visibility = View.VISIBLE
+                    binding.shimmerLayout.startShimmer()
+                }
+                is BaseResponse.Success -> {
+                    val salary = it.data?.data?.talent?.talent?.expectedSalary?.toDoubleOrNull() ?: 0.0
+                    val formatSalary = NumberFormat.getCurrencyInstance(Locale("in", "ID")).format(salary)
+                    binding.tvMyCareerGoals.text = it.data?.data?.talent?.talent?.goalCareer
+                    binding.tvMyCareerSalary.text = "Expected Salary: $formatSalary"
+                    binding.tvMyCareerGoalsDesc.text = it.data?.data?.talent?.talent?.description
+                    binding.layoutMyCareer.visibility = View.VISIBLE
+                    binding.shimmerLayout.stopShimmer()
+                    binding.shimmerLayout.visibility = View.GONE
+                }
+                is BaseResponse.Error -> {
+                    Toast.makeText(requireContext(), it.msg, Toast.LENGTH_SHORT).show()
+                } else -> {
+                    Toast.makeText(requireContext(), "Something went wrong, please try again", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     private fun setupDataRv(){
